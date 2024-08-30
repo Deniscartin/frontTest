c590e5166bf2af4c48abcacb8632db3966a48a7f
Deniscartin <cartin.denis@gmail.com>
2024-08-31 01:11:02 +0200

    test

---
+++ src/components/customers.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './customers.css';
+
+class Customers extends Component {
+  constructor() {
+    super();
+    this.state = {
+      customers: []
+    };
+  }
+
+  componentDidMount() {
+    // Sostituisci 'http://api.example.com' con il dominio fisso del tuo backend
+    const apiUrl = 'https://backtest-ktoflayq.daucu.site/api/customers';
+
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(customers => this.setState({ customers }, () => console.log('Customers fetched...', customers)))
+      .catch(error => console.error('Error fetching customers:', error));
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Customers</h2>
+        <ul>
+        {this.state.customers.map(customer => 
+          <li key={customer.id}>{customer.firstName} {customer.lastName}</li>
+        )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Customers;
